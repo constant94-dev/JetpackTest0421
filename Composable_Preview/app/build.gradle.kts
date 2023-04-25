23fd116e5ba8f659994f748510c0fa228deb794b
constant94-dev <constant94@naver.com>
2023-04-25 22:54:20 +0900

    [0425] 프로젝트 분리 및 안정화

--- Composable_Preview/app/build.gradle.kts
+++ Composable_Preview/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.test0421"
+    namespace = "com.example.test0425"
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.example.test0421"
+        applicationId = "com.example.test0425"
         minSdk = 21
         targetSdk = 33
         versionCode = 1
